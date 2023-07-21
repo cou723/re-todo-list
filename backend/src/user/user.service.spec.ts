73d8c07333b7cc007b0e3abfcc5f87d2b055e988
cou <courange.c@gmail.com>
2023-07-21 19:11:27 +0900

    Add tests for user service methods

---
+++ backend/src/user/user.service.spec.ts
@@ -0,0 +1,69 @@
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
+
+describe(' UserService', () => {
+  let service: UserService;
+  let mockRepos: Repository<User>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    mockRepos = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it('find one test', async () => {
+    const users: User[] = [
+      {
+        id: 1,
+        username: 'test',
+        password: bcrypt.hashSync('test', 10),
+      },
+      {
+        id: 2,
+        username: 'test2',
+        password: bcrypt.hashSync('test2', 10),
+      },
+    ];
+    jest.spyOn(mockRepos, 'findOne').mockResolvedValueOnce(users[0]);
+
+    const result = await service.findOne('test');
+    expect(result).toEqual(users[0]);
+  });
+
+  it('register test', async () => {
+    const username = 'test';
+    const password = 'password';
+    const hashedPassword = 'hashedPassword';
+
+    jest.spyOn(bcrypt, 'hashSync').mockReturnValue(hashedPassword);
+    jest
+      .spyOn(mockRepos, 'save')
+      .mockResolvedValueOnce({ id: 0, username, password: hashedPassword });
+
+    const result = await service.register(username, password);
+
+    expect(result).toEqual({ id: 0, username, password: hashedPassword });
+    expect(bcrypt.hashSync).toHaveBeenCalledWith(password, 10);
+    expect(mockRepos.save).toHaveBeenCalledWith({
+      username,
+      password: hashedPassword,
+    });
+  });
+});
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 
 import { User } from './user.entity';
 
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UserService {
   constructor(
@@ -12,5 +14,16 @@ export class UserService {
 
   async findOne(username: User['username']): Promise<User | undefined> {
     return await this.userRepos.findOne({ where: { username } });
+  }
+
+  async register(username, password) {
+    return await this.userRepos.save({
+      username: username,
+      password: bcrypt.hashSync(password, 10),
+    });
+  }
+
+  async delete(id: number) {
+    return await this.userRepos.delete({ id: id });
   }
 }
