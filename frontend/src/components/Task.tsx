2370db7e6ac5a34e3e1ba096fc058abd660a65a5
cou <courange.c@gmail.com>
2023-07-30 21:42:25 +0900

    Taskコンポーネント復活

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -1,21 +1,49 @@
-import { Button, Card } from 'solid-bootstrap';
+import { Card, Form } from 'solid-bootstrap';
 import { type ITaskView } from '../types/TaskView';
 import { For, createSignal } from 'solid-js';
 import TaskDeleteFailedModal from './TaskDeleteFailedModal';
 import TaskEditButtonGroup from './TaskEditButtonGroup';
+import api from '../lib/api';
 
 export const Task = (props: { class?: string; task: ITaskView }) => {
   const [showErrorModal, setShowErrorModal] = createSignal(false);
+  const [isDone, setIsDone] = createSignal(props.task.isDone);
 
   const deleteFailed = () => {
     setShowErrorModal(true);
-  }
-  return <Card class={props.class}>
-    <Card.Header><div class="d-flex justify-content-between">{props.task.title}<TaskEditButtonGroup id={props.task.id} failedDelete={deleteFailed}/></div></Card.Header>
-    <Card.Body>{props.task.description}</Card.Body>
-    <For each={props.task.children}>{(child) => <Task task={child} />}</For>
-    <TaskDeleteFailedModal show={showErrorModal()} handleClose={()=>setShowErrorModal(false)} />
-  </Card>
+  };
+
+  const handleClick = () => {
+    setIsDone(!isDone());
+    if (isDone()) api.done(props.task.id);
+    else api.undone(props.task.id);
+  };
+
+  return (
+    <Card class={props.class}>
+      <Card.Header>
+        <div class="d-flex justify-content-between">
+          <div class="ml-4">
+            <Form>
+              <Form.Check
+                type="checkbox"
+                checked={isDone()}
+                onClick={handleClick}
+              />
+            </Form>
+            <span class="h4 ml-2">{props.task.title}</span>
+          </div>
+          <TaskEditButtonGroup id={props.task.id} failedDelete={deleteFailed} />
+        </div>
+      </Card.Header>
+      <Card.Body>{props.task.description}</Card.Body>
+      <For each={props.task.children}>{(child) => <Task task={child} />}</For>
+      <TaskDeleteFailedModal
+        show={showErrorModal()}
+        handleClose={() => setShowErrorModal(false)}
+      />
+    </Card>
+  );
 };
 
 export default Task;
